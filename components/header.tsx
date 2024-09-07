3eb4475fa9026c256844454cf125cfe363a732e2
iuru2522 <ternoboy@gmail.com>
2024-09-07 16:58:59 -0400

    header component fix

--- components/header.tsx
+++ components/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backgrop-blur-[0.5rem] sm:top-6 sm:h-[3.25.rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -30,7 +30,6 @@ export default function Header() {
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
-              {/* py-3 */}
               <Link
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-5 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
